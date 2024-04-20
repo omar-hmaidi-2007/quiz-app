1f1c2281a85fbed7b18d912e51efa4f5a9dc4e23
omar-hmaidi-2007 <amroun99@gmail.com>
2024-04-20 17:06:34 +0100

    quiz app

---
+++ questions.js
@@ -0,0 +1,57 @@
+let questions = [
+    {
+        numb: 1,
+        question: "what does HTML stand for?",
+        answer: "C. Hyper text markup language",
+        options: [
+            "A. Hyper type multi language",
+            "B. Hyper text multiple language",
+            "C. Hyper text markup language",
+            "D. Home text multi language",
+        ]
+    },
+    {
+        numb: 2,
+        question: "what does CSS stand for?",
+        answer: "A. cascading style sheet",
+        options: [
+        "A. cascading style sheet",
+        "B. cute style sheet",
+        "C. computer style sheet",
+        "D. codehal style sheet",
+        ]
+    },
+    {
+        numb: 3,
+        question: "what does PHP stand for?",
+        answer: "A. Hypertext Preprocessor",
+        options: [
+            "A. Hypertext Preprocessor",
+            "B. Hometext Programming",
+            "C. Hypertext Preprogrmming",
+            "D. Progamming Hypertext Preprocessor",
+        ]
+    },
+    {
+        numb: 4,
+        question: "what does SQL stand for?",
+        answer: "D. Structured Query Language",
+        options: [
+            "A. Strenght Query Language",
+            "B. Stylesheet Query Language",
+            "C. Science Question Language",
+            "D. Structured Query Language",
+        ]
+    },
+    {
+        numb: 5,
+        question: "what does XML stand for?",
+        answer: "D. Extensible Markup Language",
+        options: [
+            "A. Excellent Multiple Language",
+            "B. Explore Multiple Language",
+            "C. Extra Markup Language",
+            "D. Extensible Markup Language",
+        ]
+    }
+];
